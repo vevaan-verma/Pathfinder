88850d5878821f43fc45660cf21d1b6482f77d8a
Vevaan <vevaan.s.verma@gmail.com>
2024-06-27 18:57:56 -0700

    Switch to rigidbody movement

--- Assets/Scripts/PathFollow.cs
+++ Assets/Scripts/PathFollow.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PathFollow : MonoBehaviour {
 
+    [Header("References")]
+    private Rigidbody rb;
+
     [Header("Path")]
     private List<Vector3Int> currPath;
     private int nextWaypoint;
 
     [Header("Settings")]
     [SerializeField] private float speed;
+    [SerializeField] private float waypointLeniency;
+
+    private void Start() {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
 
     public void StartPathFollow(List<Vector3Int> currPath) {
 
@@ -24,12 +34,18 @@ public class PathFollow : MonoBehaviour {
 
         while (nextWaypoint < currPath.Count) {
 
+            print(nextWaypoint);
             Vector3Int targetPosition = currPath[nextWaypoint]; // get next waypoint
             Vector3 direction = (targetPosition - transform.position).normalized; // get direction to next waypoint
-            transform.position += direction * speed * Time.deltaTime; // move towards next waypoint
+            rb.velocity = direction * speed; // move towards next waypoint
 
-            if (Vector3.Distance(transform.position, targetPosition) < 0.1f) // if close enough to waypoint
+            if (Vector3.Distance(transform.position, targetPosition) < waypointLeniency) { // if close enough to waypoint
+
+                rb.velocity = Vector3.zero; // stop moving
+                rb.position = targetPosition; // snap to waypoint
                 nextWaypoint++; // move to next waypoint
+
+            }
 
             yield return null;
 
